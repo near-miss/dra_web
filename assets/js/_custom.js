ed4f477931302be123ae9d9a41a71aa8a3dfd3c3
Wesley Sun <sun@nearmissmgmt.com>
2016-09-23 12:52:45 -0400

    Test

--- assets/js/_custom.js
+++ assets/js/_custom.js
@@ -38,3 +38,19 @@ $('.button-group').each( function( i, buttonGroup ) {
     $( this ).addClass('is-checked');
   });
 });
+
+
+$(function(){
+    // Check the initial Poistion of the Sticky Header
+    var stickyHeaderTop = $('#stickyheader').offset().top;
+
+    $(window).scroll(function(){
+            if( $(window).scrollTop() > stickyHeaderTop ) {
+                    $('#stickyheader').css({position: 'fixed', top: '0px'});
+                    $('#stickyalias').css('display', 'block');
+            } else {
+                    $('#stickyheader').css({position: 'static', top: '0px'});
+                    $('#stickyalias').css('display', 'none');
+            }
+    });
+});
